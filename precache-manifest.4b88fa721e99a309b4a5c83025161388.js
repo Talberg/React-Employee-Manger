e1a0ab50c8e1d342aa31d58200c4246aafafc583
Alexander Talberg <alexandermtalberg@gmail.com>
2020-02-25 12:44:26 -0700

    Updates

---
+++ precache-manifest.4b88fa721e99a309b4a5c83025161388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8997d8d29acd6835e3fd4a73c35f7282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe328d9525b43f23236c",
+    "url": "/static/css/main.37dd7484.chunk.css"
+  },
+  {
+    "revision": "c1aeaf1318fe04b440f1",
+    "url": "/static/js/2.10bac2d4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.10bac2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe328d9525b43f23236c",
+    "url": "/static/js/main.078163a6.chunk.js"
+  },
+  {
+    "revision": "56b6b8d1c48f58a3721d",
+    "url": "/static/js/runtime-main.fa40de75.js"
+  }
+]);
